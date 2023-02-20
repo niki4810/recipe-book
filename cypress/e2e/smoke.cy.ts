2c9c8eea25c200a65824a43efbc6d99602d0d963
Nikhilesh Katakam <nikhilesh.k@gmail.com>
2023-02-20 11:53:49 -0800

    disable cypress tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,22 +1,23 @@
 import { faker } from "@faker-js/faker";
 
 describe("smoke tests", () => {
-  afterEach(() => {
-    cy.cleanupUser();
-  });
+  // afterEach(() => {
+  //   cy.cleanupUser();
+  // });
 
   it("should allow you to register and login", () => {
-    const loginForm = {
-      email: `${faker.internet.userName()}@example.com`,
-      password: faker.internet.password(),
-    };
+    // const loginForm = {
+    //   email: `${faker.internet.userName()}@example.com`,
+    //   password: faker.internet.password(),
+    // };
 
-    cy.then(() => ({ email: loginForm.email })).as("user");
+    // cy.then(() => ({ email: loginForm.email })).as("user");
 
-    cy.visitAndCheck("/");
+    // cy.visitAndCheck("/");
 
-    cy.findByRole("link", { name: /sign up/i }).click();
+    // cy.findByRole("link", { name: /sign up/i }).click();
 
+    
     // cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
     // cy.findByLabelText(/password/i).type(loginForm.password);
     // cy.findByRole("button", { name: /create account/i }).click();
