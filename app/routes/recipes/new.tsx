8443a0133c7fcdd728f4f40f89210e1bd8b50a6a
Nikhilesh Katakam <nikhilesh.k@gmail.com>
2023-02-20 11:17:08 -0800

    fix ts errors

--- app/routes/recipes/new.tsx
+++ app/routes/recipes/new.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function NewRecipePage() {
   const titleRef = React.useRef<HTMLInputElement>(null);
-  const actionData = {};
+  const actionData = { errors: {title: null, body: null}};
   return (
     <div className="flex flex-col gap-4">
       <h2 className="font-bold text-xl">Add Recipe</h2>
